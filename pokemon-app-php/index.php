c5ad163133d6bf2c0b4d99fc509492345b0d3ac0
Anthony Rubio <arubio@deltec.com.co>
2025-07-28 14:58:45 -0500

    add: search for pokemon

--- pokemon-app-php/index.php
+++ pokemon-app-php/index.php
@@ -2,22 +2,34 @@
 require_once 'api/pokemonApi.php';
 require_once 'components/card.php';
 
-$pokemon = getPokemon('bulbasaur');
+if(isset($_GET["name"])) {
+  $pokemon = getPokemon($_GET["name"]);
+}
 ?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
   <meta charset="UTF-8">
   <title>Pokémon App PHP</title>
-  <link rel="stylesheet" href="css/styles.css">
+  <link rel="stylesheet" href="pokemon-app-php/css/styles.css">
 </head>
 <body>
-  <div id="app">
-    <?php if ($pokemon): ?>
-      <?= renderPokemonCard($pokemon); ?>
-    <?php else: ?>
-      <p class="error">No se pudo obtener el Pokémon.</p>
+       <form method="GET" class="search-form">
+        <label for="name">Buscar Pokémon:</label>
+        <input type="text" id="name" name="name" placeholder="Nombre del Pokémon" required>
+        <button type="submit" class="search-button">Buscar</button>
+      </form>
+      <br>
+  <?php if(isset($_GET['name'])) : ?>
+  <!-- pokemon-app-php/index.html -->
+    <div id="app">
+      <?php if ($pokemon): ?>
+        <?= renderPokemonCard($pokemon); ?>
+      <?php else: ?>
+        <p class="error">No se pudo obtener el Pokémon.</p>
+      <?php endif; ?>
+    </div>
+    <!-- pokemon-app-php/index.html -->
     <?php endif; ?>
-  </div>
 </body>
 </html>
